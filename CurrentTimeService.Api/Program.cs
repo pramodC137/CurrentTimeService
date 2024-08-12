988e0fe6e4952d256f31de06f8a5e67464ee90f0
pramodC135 <pramodmadhushan4@gmail.com>
2024-08-12 22:56:10 +0530

    add swagger and Utc time

--- CurrentTimeService.Api/Program.cs
+++ CurrentTimeService.Api/Program.cs
@@ -1,10 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 app.Run();
